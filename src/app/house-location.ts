3c737e96ccb1bcf506d60cfef15028825065d97f
Bakarijack <bakarikilu254@gmail.com>
2024-02-28 18:17:13 +0300

    interface properties added

--- src/app/house-location.ts
+++ src/app/house-location.ts
@@ -1,2 +1,10 @@
 export interface HouseLocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
 }
