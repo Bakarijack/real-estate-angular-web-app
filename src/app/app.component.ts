6b2006fd331fe4266161a053a7ca328c69cc2a7a
Bakarijack <bakarikilu254@gmail.com>
2024-02-28 16:42:48 +0300

    home component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HomeComponent],
+  template: `<main>
+    <header class="header">
+      <nav class="header-container">
+        <img src="/assets/logo.svg" alt="logo" class="brand-logo" aria-hidden="true">
+      </nav>
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>` ,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'Homes';
 }
